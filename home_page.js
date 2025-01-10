dacd8c553500f149f1425f137072394dcce994da
graden olson <gho9644@rit.edu>
2025-01-10 15:10:26 -0500

    oops

--- home_page.js
+++ home_page.js
@@ -6,20 +6,3 @@ function embed(album){
     img.setAttribute('style', 'display: none');
     embed.setAttribute('style', 'display: block');   
 }
-
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("started");
-    var apiKey = "fb16daa78d24cfa0392177c65fe39ebb";
-    var ap = new XMLHttpRequest();
-    var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=Gradono&period=7day&limit=15&api_key=" + apiKey + "&format=json";
-    var ap = new XMLHttpRequest();
-    ap.open("GET", url, true);
-    console.log(ap);
-    ap.onreadystatechange = function() {
-        console.log("hey");
-        if(ap.readyState == 4 && ap.status == 200){
-            var data = JSON.parse(ap.responseText);
-            console.log(data);
-        }
-    }
-});
