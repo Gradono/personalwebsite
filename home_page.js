d06dbdada12fc52217399a1c184cc0a42abed069
Graden Olson <79527551+Gradono@users.noreply.github.com>
2026-05-18 15:58:46 -0400

    5/18 nextjs project created

--- home_page.js
+++ home_page.js
@@ -1,8 +1,15 @@
 function embed(album){
-    console.log("embed()");
     let div = document.getElementById(album);
     let img = div.children[0];
     let embed = div.children[1];
     img.setAttribute('style', 'display: none');
     embed.setAttribute('style', 'display: block');   
+}
+
+function unembed(album){
+    let div = document.getElementById(album);
+    let img = div.children[0];
+    let embed = div.children[1];
+    img.setAttribute('style', 'display: block');
+    embed.setAttribute('style', 'display: none');   
 }
